a8b95f96d9aa3b613abd01a1cdd74da37271b044
ricodroid404 <ricodroid2024@gmail.com>
2024-03-01 18:53:30 +0900

    ポップアップを追加

--- ios_BRAIN_PUMP_UP/BrainViewController.swift
+++ ios_BRAIN_PUMP_UP/BrainViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class BrainViewController: UIViewController {
     
+    private var time:Double = 0.0
+    private var timer:Timer = Timer()
+    private var doTimer:Bool = false
+    
     // キャラ表示用のUIImageView
     @IBOutlet weak var charaImage: UIImageView!
+    // 経過時間
+    @IBOutlet weak var timeLabel: UILabel!
     
     // アニメーション用配列
     var imageArrayAttack : Array<UIImage> = []
@@ -28,21 +34,48 @@ class BrainViewController: UIViewController {
     @IBAction func attackBtn(_ sender: Any) {
         // アニメーションの適用
         charaImage.animationImages = imageArrayAttack
-        // 1.5秒間隔
-        charaImage.animationDuration = 1.5
+        // 間隔
+        charaImage.animationDuration = 2
         // 1回繰り返し
         charaImage.animationRepeatCount = 2
         // アニメーションを開始
         charaImage.startAnimating()
+        
+        startTimer()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func stop(_ sender: Any) {
+        stopTimer()
     }
-    */
-
+    
+    
+    private func startTimer() {
+        
+        if (!self.doTimer) {
+            timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [self] _ in
+                self.time += 0.01
+                let minutes = Int(self.time / 60)
+                let second = Int(self.time) % 60
+                self.timeLabel.text = String(format: "%02d:%02d", minutes, second)
+            })
+            self.doTimer = true
+        }
+       
+    }
+    
+    private func stopTimer() {
+        self.timer.invalidate()
+        self.doTimer = false
+    }
+    
+    // 筋トレ終了ボタン
+    @IBAction func endAction(_ sender: Any) {
+        // ポップアップを表示する
+        let popUpView = PopUpViewController()
+        popUpView.appear(sender: self)
+        
+        stopTimer()
+    }
+    
 }
---
+++ ios_BRAIN_PUMP_UP/PopUpViewController.swift
@@ -0,0 +1,90 @@
+//
+//  PopUpViewController.swift
+//  ios_BRAIN_PUMP_UP
+//
+//  Created by r_murata on 2024/03/01.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var contentView: UIView!
+    
+    
+    @IBAction func OnNegativeButton(_ sender: Any) {
+        hide()
+    }
+    
+    @IBAction func OnOkButton(_ sender: Any) {
+        hide()
+    }
+    
+    @IBOutlet weak var mainMessage: UILabel!
+    
+    init() {
+        super.init(nibName: "PopUpViewController", bundle: nil)
+        self.modalPresentationStyle = .overFullScreen
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textAdjust()
+        configView()
+    }
+    
+    // テキストの行間を広げる
+    private func textAdjust() {
+        let mainMessageText = "ちょっと行間にスペースを持たせるテキスト\n来週からスリランカに行く！！"
+        let lineSpacing: CGFloat = 8.0
+        let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = lineSpacing
+        
+        let attributedString = NSAttributedString(
+                string: mainMessageText,
+                attributes: [
+                    .paragraphStyle: paragraphStyle
+                ]
+            )
+        mainMessage.attributedText = attributedString
+        mainMessage.textAlignment = .center
+    }
+
+    func configView() {
+        self.view.backgroundColor = .clear
+        self.backView.backgroundColor = .black.withAlphaComponent(0.6)
+        self.backView.alpha = 0
+        self.contentView.alpha = 0
+        self.contentView.layer.cornerRadius = 10
+    }
+
+    
+    func appear(sender: UIViewController) {
+        sender.present(self, animated: false) {
+            self.show()
+        }
+    }
+    
+    private func show() {
+        UIView.animate(withDuration: 1, delay:0.1) {
+            self.backView.alpha = 1
+            self.contentView.alpha = 1
+        }
+    }
+    
+    func hide() {
+        UIView.animate(withDuration: 1, delay: 0.0, options: .curveEaseOut) {
+            self.backView.alpha = 0
+            self.contentView.alpha = 0
+        } completion: { _ in
+            self.dismiss(animated: false)
+            self.removeFromParent()
+        }
+    }
+}
