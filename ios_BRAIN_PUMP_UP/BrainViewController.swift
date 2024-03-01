3d16f34faaf28dcdc7e07e03b3bbe334c70274c7
ricodroid404 <ricodroid2024@gmail.com>
2024-03-01 17:19:23 +0900

    ナビゲーションコントローラーを配置

---
+++ ios_BRAIN_PUMP_UP/BrainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BreinViewController.swift
+//  ios_BRAIN_PUMP_UP
+//
+//  Created by r_murata on 2024/03/01.
+//
+
+import UIKit
+
+class BrainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
